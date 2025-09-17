2569bf4aaad576a74ae761df2d12cd38bcce2d2d
Jaogui08 <salesfabriciojg@gmail.com>
2025-09-17 10:48:45 -0300

    Método Construtor

---
+++ ConstrutorPessoa.java
@@ -0,0 +1,13 @@
+package backend.aula8;
+
+public class ConstrutorPessoa {
+
+	public static void main(String[] args) {
+		Pessoa musico1 = new Pessoa("Synyster Gates", 44);
+		
+		System.out.println(musico1.getNome());
+		System.out.println(musico1.getIdade());
+
+	}
+
+}
